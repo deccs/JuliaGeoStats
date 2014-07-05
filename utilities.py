36c646aa374efddb5acd2b464e0480bc9d270095
Connor Johnson <connor.labaume.johnson@gmail.com>
2014-07-05 10:39:00 -0500

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -5,28 +5,12 @@ import numpy as np
 import scipy.stats
 import matplotlib
 
-'''
-This file contains the following functions
-
-  - read_data( fn ) -> df 
-    returns a data frame
-
-  - cdf( d, bins=12 ) -> f, inv 
-    returns two NumPy arrays describing the domain and range of the CDF function and its inverse
-
-  - fit( d ) -> f 
-    returns a function that interpolates a CDF from data returned by cdf()
-
-  - to_norm( data, bins=12 ) -> z, inv, param, mu, sd 
-    returns the z-score transformed data, and other statistics for the back transformation
-
-  - from_norm( data, inv, param, mu, sd ) -> z 
-    performs the back transfromation to take data from a z-score transfrom to its original state
-'''
-
-def read_data( fn ):
+def readGeoEAS( fn ):
     '''
-    Read files of the type described in the GSLIB manual
+    Input:  (fn)   filename describing a GeoEAS file
+    Output: (data) NumPy array
+    --------------------------------------------------
+    Read GeoEAS files as described by the GSLIB manual
     '''
     f = open( fn, "r" )
     # title of the data set
@@ -70,18 +54,40 @@ def cdf( d, bins=12 ):
     Calculate the cumulative distribution function
     and its inverse for a given data set
     '''
+    # the number of data points
     N = len(d)
+    # the number of data points in each bin,
+    # and the edges for each bin
     counts, intervals = np.histogram( d, bins=bins )
+    # find the midpoints for each bin
     h = np.diff( intervals ) / 2.0
+    # iitialize arrays for a mapping and it's inverse
     f, finv = np.zeros((N,2)), np.zeros((N,2))
+    # initialize some counters and a scalar T (total)
     idx, k, T = 0, 0, float( np.sum( counts ) )
+    # for each bin..
     for count in counts:
+		# for each data point in that bin..
         for i in range( count ):
+			# let x be the midpoint of that bin
             x = intervals[idx]+h[0]
+            # let y represent the percentage of the
+            # distriution we have covered so far 
             y = np.cumsum( counts[:idx+1] )[-1] / T
-            f[k,:] = x, y 
+            # f : input value --> output percentage describing
+            # the number of points less than the input scalar
+            # in the modeled distribution; if 5 is 20% into
+            # the distribution, then f[5] = 0.20
+            f[k,:] = x, y
+            # inverse of f
+            # finv : input percentage --> output value that 
+            # represents the input percentage point of the
+            # distribution; if 5 is 20% into the distribution,
+            # then finv[0.20] = 5
             finv[k,:] = y, x
+            # increment k
             k += 1
+        # increment idx
         idx += 1
     return f, finv
     
